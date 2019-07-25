DASPRiD/Torrent-to-Web

commit 0adf32f7e49f076e123346d39f4e6ed1f82c1b13

Change chrome namespace to browser everywhere

background/main.js
@@ -49,7 +49,7 @@ torrentToWeb.processUrl = function (url, ref) {
 };
 
 torrentToWeb.createAdapter = function (callback) {
-    chrome.storage.local.get(function (options) {
+    browser.storage.local.get(function (options) {
         callback(torrentToWeb.adapter[options.adapter](
             options.url,
             options.username,
@@ -94,7 +94,7 @@ torrentToWeb.notify = function (message) {
 
         {
             type: 'basic',
-            iconUrl: chrome.extension.getURL('icons/icon-48.png'),
+            iconUrl: browser.extension.getURL('icons/icon-48.png'),
             title: 'Torrent to Web',
             message: message,
         }
@@ -102,13 +102,13 @@ torrentToWeb.notify = function (message) {
 };
 
 // Bind context menu item
-chrome.contextMenus.create({
+browser.contextMenus.create({
     id: 'send-to-torrent-client',
     title: 'Send to Torrent client',
     contexts: ['link'],
 });
 
-chrome.contextMenus.onClicked.addListener(function (info) {
+browser.contextMenus.onClicked.addListener(function (info) {
     if (info.menuItemId !== 'send-to-torrent-client') {
         return;
     }

background/adapter/qbittorrent.js
@@ -134,12 +134,12 @@ torrentToWeb.adapter.qbittorrent = function (baseUrl, username, password, autost
     }
 
     function installFilter () {
-        chrome.webRequest.onBeforeSendHeaders.addListener(
+        browser.webRequest.onBeforeSendHeaders.addListener(
             sendFilter,
             {urls: [filterUrls]},
             ['blocking', 'requestHeaders']
         );
-        chrome.webRequest.onHeadersReceived.addListener(
+        browser.webRequest.onHeadersReceived.addListener(
             receiveFilter,
             {urls: [filterUrls]},
             ['blocking', 'responseHeaders']
@@ -147,7 +147,7 @@ torrentToWeb.adapter.qbittorrent = function (baseUrl, username, password, autost
     }
 
     function removeFilter () {
-        chrome.webRequest.onHeadersReceived.removeListener(receiveFilter);
-        chrome.webRequest.onBeforeSendHeaders.removeListener(sendFilter);
+        browser.webRequest.onHeadersReceived.removeListener(receiveFilter);
+        browser.webRequest.onBeforeSendHeaders.removeListener(sendFilter);
     }
 };

commit 92d5c3e1d88c32a104163ed7afe285fade564d54

Followup for #26
0adf32f7e49f076e123346d39f4e6ed1f82c1b13 introduced a bug and
a deprecation:

1. browser.storage.local.get() returns a Promise. Handling this
   correctly now.
2. browser.extension.getURL() is deprecated. See:
   https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/extension/getURL

background/main.js
@@ -49,13 +49,15 @@ torrentToWeb.processUrl = function (url, ref) {
 };
 
 torrentToWeb.createAdapter = function (callback) {
-    browser.storage.local.get(function (options) {
+    browser.storage.local.get().then((options) => {
         callback(torrentToWeb.adapter[options.adapter](
             options.url,
             options.username,
             options.password,
             options.autostart
         ));
+    }, (error) => {
+        console.log(error);
     });
 };
 
@@ -94,7 +96,7 @@ torrentToWeb.notify = function (message) {
 
         {
             type: 'basic',
-            iconUrl: browser.extension.getURL('icons/icon-48.png'),
+            iconUrl: browser.runtime.getURL('icons/icon-48.png'),
             title: 'Torrent to Web',
             message: message,
         }