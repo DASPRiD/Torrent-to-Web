6eee78a8dcd7ef9777a37c90fd478707cd6a7534
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2026-09-24 19:50:44 +0200

    build: raise minimum firefox version to 115

--- wxt.config.ts
+++ wxt.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         browser_specific_settings: {
             gecko: {
                 id: "torrent-to-web@dasprids.de",
-                strict_min_version: "57.0",
+                strict_min_version: "115.0",
             },
         },
         permissions: [
